403ad14937d37e5c813fe7d0090189e2f0d1c1c4
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2025-01-07 20:18:52 +0100

    fixed sac

--- reinforce-the-puck/agents/sac.py
+++ reinforce-the-puck/agents/sac.py
@@ -39,9 +39,11 @@ class SACAgent(BaseAgent):
             self.alpha_optimizer = torch.optim.Adam(
                 [self._log_alpha], lr=self._config.alpha_lr
             )
-
             # use the hyperparameter from the original paper: https://arxiv.org/pdf/1812.05905 (See D Hyperparameters)
             self._target_entropy = -np.prod(action_space.shape)
+            self.alpha = self._log_alpha.exp().item()
+        else:
+            self.alpha = config.alpha
 
         self.policy = self._create_policy_net()  # Actor network
 
@@ -65,6 +67,7 @@ class SACAgent(BaseAgent):
             input_size=self._obs_dim + self._action_n,
             output_size=1,
             hidden_sizes=config.critic_hidden_sizes,
+            loss_fn=torch.nn.MSELoss(),
             learning_rate=config.trainer_config.learning_rate_critic,
         )
 
@@ -72,6 +75,7 @@ class SACAgent(BaseAgent):
             input_size=self._obs_dim + self._action_n,
             output_size=1,
             hidden_sizes=config.critic_hidden_sizes,
+            loss_fn=torch.nn.MSELoss(),
             learning_rate=config.trainer_config.learning_rate_critic,
         )
 
@@ -93,7 +97,6 @@ class SACAgent(BaseAgent):
             eps=0.000001,
         )
         self.epoch = 0
-        self.alpha = config.alpha
 
     def _policy_activation(self) -> callable:
         """Activation function for the policy network.
@@ -120,7 +123,7 @@ class SACAgent(BaseAgent):
         )
 
     def _copy_nets(self) -> None:
-        """Copy the weights from the policy and Q networks to the target networks."""
+        """Copy the policy network to the target policy network."""
         self.Q1_target.load_state_dict(self.Q1.state_dict())
         self.Q2_target.load_state_dict(self.Q2.state_dict())
 
@@ -186,13 +189,6 @@ class SACAgent(BaseAgent):
         """
         self.restore_state(torch.load(path))
 
-    def train(self, last_reward=np.nan):
-        """Train the agent."""
-        self._soft_update(self.Q1, self.Q1_target, tau=self._config.tau)
-        self._soft_update(self.Q2, self.Q2_target, tau=self._config.tau)
-
-        return super().train(last_reward)
-
     def train_step(self, batch: Batch) -> dict:
         """Perform a single training step.
 
@@ -201,38 +197,39 @@ class SACAgent(BaseAgent):
 
         Returns:
             dict: The training statistics.
+
         """
 
-        # compute the target Q value
-        with torch.no_grad():
-            next_actions, next_log_probs = self.policy.sample(batch.next_observations)
+        q1_loss, q2_loss = self.update_q_values(batch)
 
-            # select minimum Q value
-            target_q1 = self.Q1_target.Qvalues(batch.next_observations, next_actions)
-            target_q2 = self.Q2_target.Qvalues(batch.next_observations, next_actions)
-            target_q_min = torch.min(target_q1, target_q2)
-            # create bellman backup: r + gamma * (Q - alpha * log π)
-            target_q = batch.rewards + self._config.discount * (1 - batch.dones) * (
-                target_q_min - self.alpha * next_log_probs  # entropy term
-            )
+        policy_loss = self.update_policy(batch)
 
-        # update Q networks
+        if self._config.alpha_tuning:
+            alpha_loss = self.update_alpha(batch)
 
-        q1_loss = self.Q1.get_loss(
-            torch.cat([batch.observations, batch.actions], dim=1), target_q
-        )
-        q2_loss = self.Q2.get_loss(
-            torch.cat([batch.observations, batch.actions], dim=1), target_q
-        )
-        q_loss = q1_loss + q2_loss  # update both Q networks
+        self._soft_update(self.Q1, self.Q1_target, self._config.tau)
+        self._soft_update(self.Q2, self.Q2_target, self._config.tau)
 
-        self.Q1_optimizer.zero_grad()
-        self.Q2_optimizer.zero_grad()
-        q_loss.backward()
-        self.Q1_optimizer.step()
-        self.Q2_optimizer.step()
+        losses = {
+            "loss": q1_loss.item() + q2_loss.item(),
+            "actor_loss": policy_loss.item(),
+            "alpha_loss": alpha_loss.item() if self._config.alpha_tuning else 0,
+        }
+        return losses
 
-        # policy update
+    def update_alpha(self, batch):
+        _, log_probs = self.policy.sample(batch.observations)
+        alpha_loss = -(
+            self._log_alpha * (log_probs + self._target_entropy).detach()
+        ).mean()
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
+
+        self.alpha = self._log_alpha.exp().item()
+        return alpha_loss
+
+    def update_policy(self, batch):
         actions_pred, log_probs = self.policy.sample(batch.observations)
         q1_pred = self.Q1.Qvalues(batch.observations, actions_pred)
         q2_pred = self.Q2.Qvalues(batch.observations, actions_pred)
@@ -241,28 +238,41 @@ class SACAgent(BaseAgent):
         # optimize the policy to maximize the Q value and entropy
         # or minimize the negative Q value and the entropy
         # E[-(Q(s, π(s)) - α * log π(s))]
+        # negativ sign is used because torch uses gradient descent
         policy_loss = -(q_pred_min - self.alpha * log_probs).mean()
 
         self.policy_optimizer.zero_grad()
         policy_loss.backward()
         self.policy_optimizer.step()
+        return policy_loss
 
-        if self._config.alpha_tuning:
-            alpha_loss = -(
-                self._log_alpha * (log_probs.detach() + self._target_entropy).detach()
-            ).mean()
-            self.alpha_optimizer.zero_grad()
-            alpha_loss.backward()
-            self.alpha_optimizer.step()
+    def update_q_values(self, batch):
+        with torch.no_grad():
+            next_actions, next_log_probs = self.policy.sample(batch.next_observations)
 
-            self.alpha = self._log_alpha.exp().item()
+            target_q1 = self.Q1_target.Qvalues(batch.next_observations, next_actions)
+            target_q2 = self.Q2_target.Qvalues(batch.next_observations, next_actions)
+            target_q_min = torch.min(target_q1, target_q2)
 
-        losses = {
-            "loss": q_loss.item(),
-            "actor_loss": policy_loss.item(),
-            "alpha_loss": alpha_loss.item() if self._config.alpha_tuning else 0,
-        }
-        return losses
+            target_q = batch.rewards + self._config.discount * (1 - batch.dones) * (
+                target_q_min - self.alpha * next_log_probs  # entropy term
+            )
+
+        q1_loss = self.Q1.get_loss(
+            torch.cat([batch.observations, batch.actions], dim=1), target_q
+        ).mean()
+        q2_loss = self.Q2.get_loss(
+            torch.cat([batch.observations, batch.actions], dim=1), target_q
+        ).mean()
+
+        self.Q1_optimizer.zero_grad()
+        q1_loss.backward()
+        self.Q1_optimizer.step()
+
+        self.Q2_optimizer.zero_grad()
+        q2_loss.backward()
+        self.Q2_optimizer.step()
+        return q1_loss, q2_loss
 
     def __del__(self):
         return super().__del__()
