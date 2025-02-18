6c4a1e8263ff3eceba5738b436b51189b0ac2246
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2025-02-18 08:49:19 +0100

    updated report

--- reinforce-the-puck/agents/moe_agent.py
+++ reinforce-the-puck/agents/moe_agent.py
@@ -31,14 +31,17 @@ class MOEAgent(BaseAgent):
             gamma=config.gamma,
             device=self._config.specialized_config.device,
         )
+        self.last_actor = "a"
 
     def act(self, state) -> any:
         action = self.router_agent.select_action(state)
         self.last_action = action
         if action == 0:
+            self.last_actor = "a"
             with self.agent_a.evaluate_context():
                 return self.agent_a.act(state)
         else:
+            self.last_actor = "b"
             with self.agent_b.evaluate_context():
                 return self.agent_b.act(state)
 
@@ -62,6 +65,22 @@ class MOEAgent(BaseAgent):
 
         loss = self.router_agent.train_step(batch)
         loss["last_action"] = self.last_action
+
+        with self.agent_a.train_context():
+            agent_a_stats = self.agent_a.train_step(
+                self.agent_a.sample(self._batch_size)
+            )
+        with self.agent_b.train_context():
+            agent_b_stats = self.agent_b.train_step(
+                self.agent_b.sample(self._batch_size)
+            )
+
+        # concat stats
+        for key, value in agent_a_stats.items():
+            loss[f"agent_a_{key}"] = value
+        for key, value in agent_b_stats.items():
+            loss[f"agent_b_{key}"] = value
+
         return loss
 
     def state(self) -> dict:
