aad8b82f604332c8b90fc3a1512097ad1089009d
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2025-02-20 18:52:47 +0100

    fixed moe device issue

--- reinforce-the-puck/agents/moe_agent.py
+++ reinforce-the-puck/agents/moe_agent.py
@@ -134,6 +134,9 @@ class MOEAgent(BaseAgent):
         )
         agent_a_type = state["agent_a_type"]
         agent_a_config = agent_type_config_mapping[agent_a_type]()
+        agent_a_config.specialized_config.device = (
+            self._config.specialized_config.device
+        )
         agent_a_config.update_from_dict(state["agent_a_config"])
         self.agent_a = agent_type_mapping[agent_a_type](
             self._observation_space, self._action_space, agent_a_config
@@ -145,6 +148,9 @@ class MOEAgent(BaseAgent):
         )
         agent_b_type = state["agent_b_type"]
         agent_b_config = agent_type_config_mapping[agent_b_type]()
+        agent_b_config.specialized_config.device = (
+            self._config.specialized_config.device
+        )
         agent_b_config.update_from_dict(state["agent_b_config"])
         self.agent_b = agent_type_mapping[agent_b_type](
             self._observation_space, self._action_space, agent_b_config
