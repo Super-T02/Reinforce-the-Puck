917754cee4bcc0c4248b1a094046c5476ed17464
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2025-02-20 18:55:21 +0100

    fixed moe device issue

--- reinforce-the-puck/agents/moe_agent.py
+++ reinforce-the-puck/agents/moe_agent.py
@@ -129,17 +129,16 @@ class MOEAgent(BaseAgent):
             "TD3": TD3AgentConfig,
         }
 
-        print("Device: ", self._config.specialized_config.device)
-
         agent_a_states = tuple(
             state["agent_a"][f"network_{i}"] for i in range(len(state["agent_a"]))
         )
         agent_a_type = state["agent_a_type"]
         agent_a_config = agent_type_config_mapping[agent_a_type]()
+        agent_a_config.update_from_dict(state["agent_a_config"])
+
         agent_a_config.specialized_config.device = (
             self._config.specialized_config.device
         )
-        agent_a_config.update_from_dict(state["agent_a_config"])
         self.agent_a = agent_type_mapping[agent_a_type](
             self._observation_space, self._action_space, agent_a_config
         )
@@ -150,10 +149,10 @@ class MOEAgent(BaseAgent):
         )
         agent_b_type = state["agent_b_type"]
         agent_b_config = agent_type_config_mapping[agent_b_type]()
+        agent_b_config.update_from_dict(state["agent_b_config"])
         agent_b_config.specialized_config.device = (
             self._config.specialized_config.device
         )
-        agent_b_config.update_from_dict(state["agent_b_config"])
         self.agent_b = agent_type_mapping[agent_b_type](
             self._observation_space, self._action_space, agent_b_config
         )
