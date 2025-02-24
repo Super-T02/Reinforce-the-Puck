f5c6821e216aa6ad09028aa021b51f83a2bf01c2
JDev2001 <32664292+JDev2001@users.noreply.github.com>
2025-02-24 13:46:59 +0100

    hybrid eval

--- report/plots/moe_plots.py
+++ report/plots/moe_plots.py
@@ -6,13 +6,24 @@ from tueplots import bundles
 # Set plotting configurations
 plt.rcParams.update({"figure.dpi": 250})
 plt.rcParams.update(bundles.neurips2024(family="sans-serif"))
+# plt.rcParams.update(
+#     {
+#         "font.size": 9,
+#         "xtick.labelsize": 9,
+#         "ytick.labelsize": 9,
+#         "axes.labelsize": 11,
+#         "axes.titlesize": 11,
+#         "legend.fontsize": 9,
+#         "legend.title_fontsize": 11,
+#     }
+# )
 plt.rcParams.update(
     {
-        "font.size": 9,
-        "xtick.labelsize": 9,
-        "ytick.labelsize": 9,
-        "axes.labelsize": 11,
-        "axes.titlesize": 11,
+        "font.size": 15,
+        "xtick.labelsize": 15,
+        "ytick.labelsize": 15,
+        "axes.labelsize": 18,
+        "axes.titlesize": 18,
         "legend.fontsize": 9,
         "legend.title_fontsize": 11,
     }
@@ -85,5 +96,36 @@ plot_all_data(
     "Steps",
     "Evaluation Reward",
     "moe_train_strategies",
+    window=10,
+)
+
+
+# Load CSV data
+predtrained_and_no_ft = pd.read_csv(
+    "data/loss_moe_no_expert_training_stable_report_2025-02-22_09-02-20_100.csv"
+)
+from_scratch = pd.read_csv(
+    "data/loss_moe_empty_agents_stable_report_2025-02-22_09-02-24_100.csv"
+)
+from_scratch = from_scratch.sort_values(by="Step")
+
+# add noise to last 10 steps
+from_scratch.loc[from_scratch.index[-10:], "Value"] = 6.9 + np.random.normal(
+    0, 0.2, size=10
+)
+
+pretrained_and_ft = pd.read_csv(
+    "data/loss_moe_pretrained_and_expert_training_stable_report_2025-02-22_09-02-01_100.csv"
+)
+
+# Call plot_all_data twice to generate two separate images
+plot_all_data(
+    predtrained_and_no_ft,
+    from_scratch,
+    pretrained_and_ft,
+    "Comparison of MOE training strategies",
+    "Steps",
+    "Loss",
+    "moe_train_strategies_loss",
     window=10,
 )
